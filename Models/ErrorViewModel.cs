6f9fd29c24b0642382929dc5677e5677452ced99
Milos Katinski <mkatinski@mirabeau.nl>
2020-03-12 22:04:53 +0100

    initial deployment

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DevOpsDemo_SuperAdmins.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DevOpsDemo_SuperAdmins
+
+[![Deploy to Azure](https://azuredeploy.net/deploybutton.png)](https://azuredeploy.net/)
