0608f2af77282bfa8fffc9a6fd8aeb6c3df5e666
Milos Katinski <mkatinski@mirabeau.nl>
2020-03-12 23:30:19 +0100

    test change

--- HelloWorldMvc/Properties/AssemblyInfo.cs
+++ HelloWorldMvc/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HelloWorldMvc")]
+[assembly: AssemblyTitle("Azure DevOps demo by MKatinski")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HelloWorldMvc")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("HelloWorld")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
